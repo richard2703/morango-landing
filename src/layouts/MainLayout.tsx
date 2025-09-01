1668e08a64ddcb1e9c82c93d89b02acdf12b6f42
Ricardo Rios <richard.2703.rr@gmail.com>
2025-09-01 16:58:39 -0600

    franquicias casi completo

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -9,6 +9,7 @@ type Props = {
 const MainLayout: React.FC<Props> = ({ children }) => {
   return (
     <div className="flex flex-col min-h-screen ">
+      <title>Morango</title>
       <Header />
       <main className="flex-grow">{children}</main>
       <Footer />
