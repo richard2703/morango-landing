d8efc0214d6609ffc8c84fd4aa2545f9a9390034
Ricardo Rios <richard.2703.rr@gmail.com>
2025-09-01 18:00:07 -0600

    actualizacion

--- src/pages/Franquicias.tsx
+++ src/pages/Franquicias.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { Store, Clock, Mail, Phone, MapPin, ArrowRight } from 'lucide-react';
+import  { useState } from 'react';
+import { Store, Mail  } from 'lucide-react';
 
 const Franquicias = () => {
   const [email, setEmail] = useState('');
